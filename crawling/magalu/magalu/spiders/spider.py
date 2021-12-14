84b440c9e4cae5c16b8251ff51bd72dc45c5438a
wellinton <wellintonpiassa@hotmail.com>
2021-12-14 03:12:58 -0300

    Creating parse_iphone and parse_ratings

--- crawling/magalu/magalu/spiders/spider.py
+++ crawling/magalu/magalu/spiders/spider.py
@@ -19,12 +19,47 @@ class MagaluSpider(scrapy.Spider):
             yield scrapy.Request(next_page, callback=self.parse_home_page)
 
 
-
     def parse_iphone_page(self, response):
         store_infos = self.parse_store(response)
-        #iphone_infos = parse_iphone()
-        #duvidas_infos = parse_duvidas()
-        #rating_infos = parse_ratings()
+        iphone_infos = self.parse_iphone(response)
+        rating_infos = self.parse_ratings(response)
+
+        yield {
+            'loja' : {
+                'nome': store_infos[0],
+                'endereco': store_infos[1],
+                'telefone': store_infos[2],
+                'nome_logo': store_infos[3]
+            },
+
+            'iphone' : {
+                'cod': iphone_infos[0],
+                'nome_loja': iphone_infos[1],
+                'link_iphone': iphone_infos[2],
+                'link_imagem':iphone_infos[3],
+                'titulo':iphone_infos[4],
+                'preco_avista': iphone_infos[5],
+                'preco_aprazo': iphone_infos[6],
+                'cor': iphone_infos[7],
+                'tam_tela': iphone_infos[8],
+                'resolucao_cam_front': iphone_infos[9],
+                'resolucao_cam_tras': iphone_infos[10],
+                'mem_int': iphone_infos[11],
+                'mem_ram': iphone_infos[12]
+            },
+
+            'avaliacao' : {
+                'titulo' : rating_infos[0],
+                'descricao' : rating_infos[1],
+                'data' : rating_infos[2],
+                'avaliador_nome' : rating_infos[3],
+                'likes' : rating_infos[4],
+                'deslikes' : rating_infos[5],
+                'nota' : rating_infos[6],
+                'iphone_cod ': rating_infos[7],
+                'nome_loja' : rating_infos[8]
+            }
+        }
         
 
     def parse_store(self, response):
@@ -39,4 +74,69 @@ class MagaluSpider(scrapy.Spider):
         #nome-logo
         infos.append(infos[0].replace(" ","").lower() + ".jpg")
 
+        print(infos)
         return infos
+
+    
+    def parse_iphone(self, response):
+        infos = []
+        # Cod
+        infos.append(response.css('.header-product__code::text').get())
+        # nome_loja
+        infos.append(response.css('.container-left-top-header a::text').get())
+        # link_iphone
+        infos.append(response.url)
+        # link_imagem
+        infos.append(response.css('.showcase-product__container-img a img::attr(src)').get())
+        # titulo
+        infos.append(response.css('.header-product__title::text').get())
+        # preco_avista
+        infos.append(response.css('.price-template__text::text').get())
+        # preco_aprazo
+        infos.append(response.css('.price-template::text')[2].get())
+        # cor
+        #infos.append()
+        # tam_tela
+        #infos.append()
+        # resolucao_cam_front
+        #infos.append()
+        # resolucao_cam_tras
+        #infos.append()
+        # mem_int
+        #infos.append()
+        #mem_ram
+
+        print(infos)
+        return infos
+
+
+    def parse_ratings(self, response):
+        infos = []
+
+        # titulo
+        infos.append(response.css('.product-review__text-content--title::text').get())
+        # descricao
+        infos.append(response.css('.product-review__post p::text').get())
+        # data
+        infos.append(response.css('.product-review__text-highlight::text').get())
+        # avaliador_nome
+        infos.append(response.css('.product-review__text-content::text').get())
+        # likes
+        #infos.append(response.css('.product-review__text-highlight.product-review__spacing::text').get())
+        #deslikes
+        #infos.append(response.css('.product-review__text-highlight.product-review__spacing::text').get())
+        # nota
+        #infos.append()
+        # iphone_cod
+        infos.append(response.css('.header-product__code::text').get())
+        # loja_nome
+        infos.append(response.css('.container-left-top-header a::text').get())
+
+        print(infos)
+        return infos
+
+# BUGS 
+        # likes e deslikes com mesmo nome de classes
+        # Como pegar os dados das tabelas de especificações
+        # Como pegar a nota da avaliacao
+        # Fazer loop nas avaliacoes
