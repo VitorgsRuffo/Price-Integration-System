386b61ac121710e4502870d394c1321f039f745d
wellinton <wellintonpiassa@hotmail.com>
2021-12-14 02:14:50 -0300

    Creating parse_store

--- crawling/magalu/magalu/spiders/spider.py
+++ crawling/magalu/magalu/spiders/spider.py
@@ -11,11 +11,32 @@ class MagaluSpider(scrapy.Spider):
     def parse_home_page(self, response):
 
         for magalu in response.css('#showcase ul a::attr(href)'):
-            yield {
-                'link': magalu.get()
-            }
+            yield scrapy.Request(magalu.get(), callback=self.parse_iphone_page)
 
         next_page = response.css('.css-1a9p55p.css-197gxuo + .css-1a9p55p a::text').get()
         if next_page is not None:
             next_page = response.url[:-1] + next_page
             yield scrapy.Request(next_page, callback=self.parse_home_page)
+
+
+
+    def parse_iphone_page(self, response):
+        store_infos = self.parse_store(response)
+        #iphone_infos = parse_iphone()
+        #duvidas_infos = parse_duvidas()
+        #rating_infos = parse_ratings()
+        
+
+    def parse_store(self, response):
+        infos = []
+
+        # nome da loja
+        infos.append(response.css('.container-left-top-header a::text').get())
+        # Endereco
+        infos.append(response.css('.bg-footer-address::text').get())
+        #telefone
+        infos.append(response.css('.phone-buyphone::text').get())
+        #nome-logo
+        infos.append(infos[0].replace(" ","").lower() + ".jpg")
+
+        return infos
